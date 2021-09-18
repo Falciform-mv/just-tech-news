92d13f6f22fa8bf9993cc0bf57ded8532e8a4ad6
Mike Veneziale <falciformdesign@gmail.com>
2021-09-18 17:22:19 -0500

    html is dynamically updated with 1 request

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,17 @@ const router = require('express').Router();
 // sets up main homepage route
 router.get('/', (req, res) => {
   // renders a file you can choose which file by putting in parentheses
-  res.render('homepage');
+  res.render('homepage', {
+    id: 1,
+    post_url: 'https://handlebarsjs.com/guide/',
+    title: 'handlebars docs',
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: 'test_user'
+    }
+  });
 });
 
 
