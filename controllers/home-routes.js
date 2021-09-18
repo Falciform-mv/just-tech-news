276212b0146d9e9ea2aa4520e372498c8c64df35
Mike Veneziale <falciformdesign@gmail.com>
2021-09-18 16:55:16 -0500

    updated changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,9 @@
 
 const router = require('express').Router();
 
+// sets up main homepage route
 router.get('/', (req, res) => {
+  // renders a file you can choose which file by putting in parentheses
   res.render('homepage');
 });
 
