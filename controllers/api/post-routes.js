83457436ab5ba593e723521df1a01f413479f18c
Mike Veneziale <falciformdesign@gmail.com>
2021-09-21 11:01:26 -0500

    conditionally render features for logged in users only

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -90,13 +90,16 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+  // make sure session exists first
+  if (req.session) {
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
     .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+  }
+  // custom static method created in models/Post.j
 });
 
 router.put('/:id', (req, res) => {
